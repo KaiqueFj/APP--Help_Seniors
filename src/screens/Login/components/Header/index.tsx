f380110aba3fbe83a723a630e22cd165ae13d321
Kaique Campos <kaiquecamposferreira@gmail.com>
2021-09-17 21:56:01 -0300

    Login page design ready

--- src/screens/Login/components/Header/index.tsx
+++ src/screens/Login/components/Header/index.tsx
@@ -1,21 +1,27 @@
+import { useNavigation } from "@react-navigation/core";
 import React from "react";
 import { Linking, Text, TextInput, View } from "react-native";
 import { style } from './styles';
 
 export function Header() {
+
+    const navigation = useNavigation()
+
     return (
         <View style={style.container}>
 
             <View>
                 <Text style={style.title}>Faça seu login</Text>
-                <Text
-                    style={style.link}
-                    onPress={() => Linking.openURL('http://google.com')}> Login</Text>
+
+                <Text style={style.legend}>
+                    Caso tenha não tenha uma conta, 
+
+                    <Text
+                        style={style.link}
+                        onPress={() => navigation.navigate('Register')}> registre-se</Text>
+                </Text>
+
             </View>
-
-            <TextInput>Digite seu e-mail:</TextInput>
-            <TextInput>*********</TextInput>
-
         </View>
     )
 }
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -1,5 +1,65 @@
 import { StyleSheet } from "react-native"
+import {theme} from '../../global/styles/theme'
 
 export const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
 
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    inputContainer: {
+        width: '85%',
+        height: 65,
+
+        position: 'relative',
+
+        flex: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+
+        marginTop: 10
+    },
+
+    inputArea: {
+        width: '100%',
+        height: '100%',
+        paddingLeft: 65,
+
+        fontSize: 25,
+        fontFamily: theme.fonts.normal400,
+        color: theme.colors.purple,
+
+        borderRadius: 15,
+        borderBottomWidth: 4,
+        borderRightWidth: 1,
+        borderLeftWidth: 1,
+
+        borderColor: theme.colors.purple,
+
+        backgroundColor: theme.colors.second
+    },
+
+    button: {
+        width: '85%',
+        height: 65,
+
+        overflow: 'hidden',
+        borderRadius: 15,
+
+        marginTop: 50,
+    },
+
+    linearGradient: {
+        width: '100%',
+        height: 65,
+
+        flex: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 })
